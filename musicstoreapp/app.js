a6e21f820b7c7c92dc383574f40d9f53552d2642
UO282337 <UO282337@uniovi.es>
2023-03-22 10:19:42 +0100

    SDI-2223-112-8.1-Acceso a datos con Mongodb desde Node.js.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -10,7 +10,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
-require("./routes/songs.js")(app);
+const { MongoClient } = require("mongodb");
+const url = 'mongodb+srv://admin:sdi@eii-sdi-cluster.uzjjiv6.mongodb.net/?retryWrites=true&w=majority'
+app.set('connectionStrings', url);
+require("./routes/songs.js")(app, MongoClient);
 require("./routes/authors.js")(app);
 
 // view engine setup
