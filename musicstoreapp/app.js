a76d62d85a94e059738177efa021bbcf68740bb7
UO282337 <UO282337@uniovi.es>
2023-03-22 10:27:16 +0100

    SDI-2223-112-8.2-Arquitectura para el acceso a datos.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -13,7 +13,9 @@ let usersRouter = require('./routes/users');
 const { MongoClient } = require("mongodb");
 const url = 'mongodb+srv://admin:sdi@eii-sdi-cluster.uzjjiv6.mongodb.net/?retryWrites=true&w=majority'
 app.set('connectionStrings', url);
-require("./routes/songs.js")(app, MongoClient);
+let songsRepository = require("./repositories/songsRepository.js");
+songsRepository.init(app, MongoClient);
+require("./routes/songs.js")(app, songsRepository);
 require("./routes/authors.js")(app);
 
 // view engine setup
